b575d5cdf56b0b964fbfb668363b3ce16c683a53
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2018-11-04 10:42:55 +0200

    Empty template

---
+++ Template/NewspaperSellerSimulation/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewspaperSellerTesting;
+
+namespace NewspaperSellerSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+
+            var system = TestingManager.GetReferenceSystem(Constants.FileNames.TestCase1);
+
+
+            var result = TestingManager.Test(system, Constants.FileNames.TestCase1);
+            MessageBox.Show(result);
+        }
+    }
+}
