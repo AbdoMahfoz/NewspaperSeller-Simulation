f9e7aca559744eede41123f189eb72ae207e3821
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2018-11-09 20:25:19 +0200

    Clean-up

--- Template/NewspaperSellerSimulation/Program.cs
+++ Template/NewspaperSellerSimulation/Program.cs
@@ -18,24 +18,9 @@ namespace NewspaperSellerSimulation
         [STAThread]
         static void Main()
         {
-            SimulationSystem system = TestCaseManager.FromFile(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory().ToString()).ToString()) + "\\TestCases\\TestCase3.txt");
-            SimulationSystem system2 = (SimulationSystem)system.Clone();
-            long ParallelTime, SequentialTime;
-            Stopwatch watch = new Stopwatch();
-            watch.Restart();
-            Igniter.ParallelRun(system2);
-            watch.Stop();
-            ParallelTime = watch.ElapsedMilliseconds;
-            watch.Restart();
-            Igniter.SequntialRun(system);
-            watch.Stop();
-            SequentialTime = watch.ElapsedMilliseconds;
-            MessageBox.Show(TestingManager.Test(system, "TestCase3.txt") + "\nParallel time = " + ParallelTime + ", Sequntial time = " + SequentialTime);
-            /*
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            */
         }
     }
 }
